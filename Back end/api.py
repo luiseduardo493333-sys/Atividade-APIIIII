62c8db791b0c026746f5537c617a48f6199f4a9a
Luis <Luiseduardo493333@gmail.com>
2025-10-20 10:08:25 -0300

    Função listar roupas

--- Back end/api.py
+++ Back end/api.py
@@ -1,4 +1,23 @@
 from fastapi import FastAPI
 import funcao
 
-app = FastAPI(title= " Gerenciador de Filmes")
+app = FastAPI(title="Loja de Roupas")
+
+@app.get("/")
+def home():
+    return {"Mensagem": "Bem-vindo à Loja!"}
+
+@app.get("/roupas")
+def catalogo():
+    roupas = funcao.listar_roupas()
+    lista = []
+    for roupa in roupas:
+        lista.append({
+            "id": roupa[0],
+            "nome": roupa[1],
+            "categoria": roupa[2],
+            "tamanho": roupa[3],
+            "cor": roupa[4],
+            "preco": roupa[5],
+            "estoque": roupa[6]
+        })
